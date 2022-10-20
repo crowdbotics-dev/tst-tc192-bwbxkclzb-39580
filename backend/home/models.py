dff2b9c43584a939387eafe843935e49b0af3312
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 11:29:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Addressfsxopvglpy(models.Model):
     "Generated Model"
     street = models.BigIntegerField()
+    ca = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
